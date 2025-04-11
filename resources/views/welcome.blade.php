f27716d1ee6f72c99156eba5b19dac205e7f83b7
Ahmad Faisal <maulidae.rizki@gmail.com>
2025-04-11 17:39:37 +0700

    2nd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                                             <h2 class="text-xl font-semibold text-black dark:text-white">Documentation</h2>
 
                                             <p class="mt-4 text-sm/relaxed">
-                                                Testing CI/CD pipelines for your Web Blog Laravel application Part IX.
+                                                AP
                                             </p>
                                         </div>
                                     </div>
